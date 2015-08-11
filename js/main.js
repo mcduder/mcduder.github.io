3759e80a1dfc6a54e83ba6e7e72bca18df19ce7c
Mcduder <mcblood1@gmail.com>
2015-08-10 22:24:08 -0700

    Redid site

--- js/main.js
+++ js/main.js
@@ -1,23 +1,17 @@
-// Gumby is ready to go
-Gumby.ready(function() {
-	Gumby.log('Gumby is ready to go...', Gumby.dump());
-
-	// placeholder polyfil
-	if(Gumby.isOldie || Gumby.$dom.find('html').hasClass('ie9')) {
-		$('input, textarea').placeholder();
-	}
-
-	// skip link and toggle on one element
-	// when the skip link completes, trigger the switch
-	$('#skip-switch').on('gumby.onComplete', function() {
-		$(this).trigger('gumby.trigger');
-	});
-
-// Oldie document loaded
-}).oldie(function() {
-	Gumby.warn("This is an oldie browser...");
-
-// Touch devices loaded
-}).touch(function() {
-	Gumby.log("This is a touch enabled device...");
+$(document).ready(function(){
+	resizeDiv();
 });
+
+window.onresize = function(event) {
+	resizeDiv();
+}
+
+function resizeDiv() {
+	vpw = $(window).width();
+	vph = $(window).height();
+	$('.header').css({'height': vph + 'px'});
+}
+
+function fadeIn() {
+	$('body').fadeIn(1500);
+}
