6ea5930348c341aa1bff0b7d6bc4d298c4d03b7b
Azeem <mcblood1@gmail.com>
2019-09-07 09:51:48 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$("body").on("contextmenu", "img", function(e) {
   		return false;
 	});
+	console.log('yes');
 });
 
 window.onresize = function(event) {
@@ -17,4 +18,19 @@ function resizeDiv() {
 
 function fadeIn() {
 	$('body').fadeIn(1500);
+}
+
+function s(option) {
+	let e = document.getElementById(option);
+	console.log(option);
+	e.scrollIntoView();
+}
+
+function min() {
+	let x = document.getElementById('navbar');
+	if (x.className === 'navbar') {
+		x.className += " responsive";
+	} else {
+		x.className = "navbar";
+	}
 }
